9b656959e6c20d7d73b7cf5cf394c2da21269725
DESKTOP-M3EH82I\Casey <casey.haralson@gmail.com>
2022-07-17 15:51:09 -0600

    adding ability to load json dictionary file and then process the data into a new file

---
+++ editor/Editor/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Editor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var dictionaryFolderPath = "../../../../../data/";
+
+            string filename = "a.json";
+
+
+            using FileStream openStream = File.OpenRead(dictionaryFolderPath + filename);
+            using StreamReader sr = new StreamReader(openStream);
+            var text = sr.ReadToEnd();
+            var test = JsonConvert.DeserializeObject<dynamic>(text);
+
+
+            var sw = new StreamWriter(filename.Replace("json", "txt"));
+            foreach (var item in (Newtonsoft.Json.Linq.JObject)test)
+            {
+                sw.WriteLine(item.Key);
+            }
+
+
+            var d = "";
+        }
+    }
+}
